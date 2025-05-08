57e2b938064bad2e7f743a6b9b9954453af070ea
Aditya Wadkar <aditya.wadkar@volvo.com>
2025-05-08 10:27:10 +0530

    created AuthRepository for database services

---
+++ src/main/java/com/example/authservice/repository/AdminRepository.java
@@ -0,0 +1,8 @@
+package com.example.authservice.repository;
+
+import com.example.authservice.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin,Long> {
+    boolean existsByEmail();
+}
