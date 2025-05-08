ad5513898679aba362855ffe92f18562900d6717
Aditya Wadkar <aditya.wadkar@volvo.com>
2025-05-08 13:38:56 +0530

    modified adminService implemented businnes logic for adminAuthentication endpoint

--- src/main/java/com/example/authservice/service/AdminService.java
+++ src/main/java/com/example/authservice/service/AdminService.java
@@ -1,5 +1,6 @@
 package com.example.authservice.service;
 
+import com.example.authservice.dto.LoginRequest;
 import com.example.authservice.dto.SignupRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -7,4 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 public interface AdminService {
     ResponseEntity<?> registerAdmin(@Valid @RequestBody SignupRequest signUpRequest);
+
+    ResponseEntity<?> adminAuthetication(@Valid LoginRequest loginRequest);
 }
