daed292a5640732b258139365c91e3c3d5869838
Aditya Wadkar <aditya.wadkar@volvo.com>
2025-05-08 13:36:32 +0530

    Created UserDetailsImp for fetching the user details

--- src/main/java/com/example/authservice/security/UserDetailsImpl.java
+++ src/main/java/com/example/authservice/security/UserDetailsImpl.java
@@ -1,4 +1,23 @@
 package com.example.authservice.security;
 
+import com.example.authservice.model.Admin;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDetailsImpl {
+    private Long id;
+    private String email;
+    private String password;
+
+    public static UserDetailsImpl build(Admin admin){
+        return new UserDetailsImpl(admin.getAdminId(), admin.getAdminEmail(), admin.getPassword());
+    }
+
+
 }
