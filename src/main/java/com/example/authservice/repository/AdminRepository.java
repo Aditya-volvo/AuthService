a5ce65c8f719be33f9650db5ca4f828873784b31
Aditya Wadkar <aditya.wadkar@volvo.com>
2025-05-09 16:30:36 +0530

    I have added method find ByEmail()

--- src/main/java/com/example/authservice/repository/AdminRepository.java
+++ src/main/java/com/example/authservice/repository/AdminRepository.java
@@ -3,6 +3,10 @@ package com.example.authservice.repository;
 import com.example.authservice.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdminRepository extends JpaRepository<Admin,Long> {
     boolean existsByEmail();
+
+    Optional<Admin> findByEmail(String email);
 }
