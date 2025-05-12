380876c1cb33941315c2772a3871126bf47f0890
Aditya Wadkar <aditya.wadkar@volvo.com>
2025-05-12 10:34:34 +0530

    resloved error for AuthTokenFilter

--- src/main/java/com/example/authservice/config/SecurityConfig.java
+++ src/main/java/com/example/authservice/config/SecurityConfig.java
@@ -1,23 +1,28 @@
 package com.example.authservice.config;
 
-import com.example.authservice.security.AuthEntryPointJwt;
+import com.example.authservice.security.AuthTokenFilter;
 import com.example.authservice.security.UserDetailsServiceImpl;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class SecurityConfig {
     private  final UserDetailsServiceImpl userDetailsService;
-    private  final AuthEntryPointJwt unAuthorizedHandler;
+    private  final AuthTokenFilter unAuthorizedHandler;
 
     @Bean
     public AuthTokenFilter authenticationJwtTokenFilter(){
@@ -32,12 +37,15 @@ public class SecurityConfig {
         return authConfig.getAuthenticationManager();
     }
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(unAuthorizedHandler)
-                .and().authorizeHttpRequests().requestMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated();
-
-        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.securityMatcher("/**")
+                .csrf(csrf -> csrf.disable())
+                .exceptionHandling(ex->ex.authenticationEntryPoint((AuthenticationEntryPoint) unAuthorizedHandler))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/api/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+        .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
